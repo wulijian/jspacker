f83b7ef90ecb2750600362f7f7a8c0834f262ea0
wulijian <wulijian722@gmail.com>
2013-03-03 16:47:33 +0800

    发布commonJS打包工具

---
+++ Readme.md
@@ -0,0 +1,4 @@
+支持 commonJS 类模块化开发项目的静态打包，暂时只部分支持 seaJS 类项目的打包.
+## Contributors
+authors  : KnightWu
+## Links
---
+++ index.js
@@ -0,0 +1,6 @@
+/**
+ * @describe:
+ * @author: KnightWu
+ * @version: 0.0.1
+ */
+module.exports = require('./lib/main');
